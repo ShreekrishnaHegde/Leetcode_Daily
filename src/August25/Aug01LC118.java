d5695b4cb1897034fef079318a6fbbbcb0e135fb
Shreekrishna Hegde <hegdeshreekrishna469@gmail.com>
2025-08-08 23:44:49 +0530

    msg

--- src/August25/Aug01LC118.java
+++ src/August25/Aug01LC118.java
@@ -2,7 +2,12 @@ package August25;
 
 import java.util.ArrayList;
 import java.util.List;
+/*
+Given an integer numRows, return the first numRows of Pascal's triangle.
 
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+https://leetcode.com/problems/pascals-triangle/description/?envType=daily-question&envId=2025-08-01
+ */
 public class Aug01LC118 {
     public static void main(String[] args) {
 
@@ -25,6 +30,34 @@ public class Aug01LC118 {
             ans.add(row);
         }
         return ans;
+    }
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<List<Integer>>();
+
+        triangle.add(new ArrayList<>());
+        triangle.get(0).add(1);
+
+        for (int rowNum = 1; rowNum < numRows; rowNum++) {
+            List<Integer> row = new ArrayList<>();
+            List<Integer> prevRow = triangle.get(rowNum - 1);
+
+            // The first row element is always 1.
+            row.add(1);
+
+            // Each triangle element (other than the first and last of each row)
+            // is equal to the sum of the elements above-and-to-the-left and
+            // above-and-to-the-right.
+            for (int j = 1; j < rowNum; j++) {
+                row.add(prevRow.get(j - 1) + prevRow.get(j));
+            }
+
+            // The last row element is always 1.
+            row.add(1);
+
+            triangle.add(row);
+        }
+
+        return triangle;
     }
 
 }
